fd4a9aba1d7652fecd9eafbf5701300885811f9d
Maccharmy <musanga@byupathway.edu>
2026-07-07 12:24:28 +0100

    wk1 final

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,9 +1,1 @@
 document.getElementById("lastModified").textContent = document.lastModified;
-// This is a single-line comment
-
-/*
-This is a
-multi-line comment
-*/
-
-console.log("Hello World"); // 
